925371821cddc285d89b4dbdfcba9b78da11003e
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-12-02 18:52:17 +0300

    Replace map txt files with json files

--- Utils/MapParser.cs
+++ Utils/MapParser.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualBasic;
+﻿using Newtonsoft.Json;
 using ReachTheFlag.Cells;
 using ReachTheFlag.Structure;
 
@@ -9,11 +9,14 @@ namespace ReachTheFlag.Game
         private static int _flagCellsCount = 0;
         private static int _playerCellsCount = 0;
 
-        private static string[] readBoardMapFile(string filename)
+        private static string readBoardMapFile(string filename)
         {
             if (File.Exists(filename))
             {
-                return File.ReadAllLines(filename);
+                using (StreamReader r = new StreamReader(filename))
+                {
+                    return r.ReadToEnd();
+                }
             }
 
             throw new FileNotFoundException("File " + filename + " was not found.");
@@ -26,6 +29,12 @@ namespace ReachTheFlag.Game
 
             if (_playerCellsCount > 1) throw new Exception("Please provide only one player cell.");
             if (_flagCellsCount > 1) throw new Exception("Please provide only one flag cell.");
+        }
+
+        private static void resetFlagAndPlayerCellsCount()
+        {
+            _flagCellsCount = 0;
+            _playerCellsCount = 0;
         }
 
         private static (string cellType, int allowedNumberOfSteps, int weight) getCellInfo(string cellString)
@@ -53,23 +62,25 @@ namespace ReachTheFlag.Game
 
         public static GameBoard ParseBoardMap(string boardFilename)
         {
-            string[] board = readBoardMapFile(boardFilename);
+            string json = readBoardMapFile(boardFilename);
+            var board = JsonConvert.DeserializeObject<string[][]>(json);
+
             BoardCell[][] cellsArray = new BoardCell[board.Length][];
 
             for (int i = 0; i < board.Length; i++)
             {
-                string[] cellsRow = board[i].Split(",");
-                cellsArray[i] = new BoardCell[cellsRow.Length];
+                cellsArray[i] = new BoardCell[board[i].Length];
 
-                for (var j = 0; j < cellsRow.Length; j++)
+                for (var j = 0; j < board[i].Length; j++)
                 {
-                    (string cellType, int allowedNumberOfSteps, int weight) = getCellInfo(cellsRow[j]);
+                    (string cellType, int allowedNumberOfSteps, int weight) = getCellInfo(board[i][j]);
                     validateFlagAndPlayerCellsCount(cellType);
 
                     cellsArray[i][j] = CellFactory.GetCell(i, j, cellType, allowedNumberOfSteps, weight);
                 }
             }
 
+            resetFlagAndPlayerCellsCount();
             return new GameBoard(cellsArray);
         }
     }
